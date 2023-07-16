4dfdcc2e6221c3ce53a6de058431da9929d3c2af
mloisking <126358439+mloisking@users.noreply.github.com>
2023-07-16 12:41:40 -0400

    added new codes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,47 @@
 
 import React from 'react';
-import {useState} from 'react'
+import { useState } from 'react'
 
 
 //Write color component
-const Color = (props) => {
-  return <div className= {props.color}></div>
+
+const Color = ({ color, setSelectedColor }) => {
+  return (
+    <div
+      className={color}
+      onClick={() => setSelectedColor(color)}
+    ></div>
+  )
 }
 
 // Picker component here
 function Picker() {
-const [selectedColor, selectedColor]=useState("")
- return (
-   <Picker>
-<div>
-<div id="navbar">
-  <div>Currently selected: </div>
-  <div className={selectedColor}>{selectedColor}</div>
-</div>
- </div> 
-</Picker>
-)
- };
+  const [selectedColor, setSelectedColor] = useState("")
+  return (
+    <Picker>
+      <div id="colors-list">
+        <Color color="red" setSelectedColor={setSelectedColor} />
+        <Color color="blue" setSelectedColor={setSelectedColor} />
+        <Color color="green" setSelectedColor={setSelectedColor} />
+        <Color color="red" selectedColor={selectedColor} />
+        <Color color="blue" selectedColor={selectedColor} />
+        <Color color="green" selectedColor={selectedColor} />
+      </div>
+    </Picker>
+  )
+};
 
 const App = () => {
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className="blue">blue</div>
       </div>
       <div id="colors-list">{/* colors go here */}
-      <Color color="blue"/>
-      <Color color="green"/>
-      <Color color="red"/>
+        <Color color="blue" />
+        <Color color="green" />
+        <Color color="red" />
       </div>
     </div>
   )
