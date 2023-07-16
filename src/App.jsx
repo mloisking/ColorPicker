b668dc6e998e4910d648bb8a526aec2fcf01073c
mloisking <126358439+mloisking@users.noreply.github.com>
2023-07-16 11:54:14 -0400

    added new codes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,27 @@
-// Write your Color component here
+
+import React from 'react';
+import {useState} from 'react'
+
+
+//Write color component
+const Color = (props) => {
+  return <div className= {props.color}></div>
+}
+
+// Picker component here
+function Picker() {
+const [selectedColor, selectedColor]=useState("")
+ return (
+   <Picker>
+<div>
+<div id="navbar">
+  <div>Currently selected: </div>
+  <div className={selectedColor}>{selectedColor}</div>
+</div>
+ </div> 
+</Picker>
+)
+ };
 
 const App = () => {
   return (
@@ -7,9 +30,14 @@ const App = () => {
         <div>Currently selected: </div>
         <div className="red">red</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">{/* colors go here */}
+      <Color color="blue"/>
+      <Color color="green"/>
+      <Color color="red"/>
+      </div>
     </div>
-  );
+  )
 };
+
 
 export default App;
---
+++ src/components/picker.jsx
@@ -0,0 +1,13 @@
+function Picker(Color) {
+const Picker=(){
+    return (
+        <picker>
+            <div id="colors-list">
+                <Color />
+                <Color />
+                <Color />
+                
+            </div>
+        </picker>
+    )
+    }
